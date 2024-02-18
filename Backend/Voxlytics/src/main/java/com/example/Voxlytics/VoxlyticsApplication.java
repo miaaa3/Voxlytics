2abe6a598ff3e3d3b6676766481d8654b0206f50
miaa <belmouaddine.ptech@gmail.com>
2024-02-18 12:55:48 +0100

    backend

---
+++ Backend/Voxlytics/src/main/java/com/example/Voxlytics/VoxlyticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.Voxlytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoxlyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoxlyticsApplication.class, args);
+	}
+
+}
---
+++ Backend/Voxlytics/src/test/java/com/example/Voxlytics/VoxlyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Voxlytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoxlyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
